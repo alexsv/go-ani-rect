5bea4fcfbbc955b8254a99255059a7ec0d7419d6
Alex Vasilenko <alex@localhost.com>
2016-02-23 09:11:53 +0100

    added main screen initialization

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/jroimartin/gocui"
+)
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if _, err := g.SetView("main", -1, -1, maxX, maxY); err != nil {
+		if err := g.SetCurrentView("main"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func main() {
+	g := gocui.NewGui()
+	if err := g.Init(); err != nil {
+		panic(err)
+	}
+	defer g.Close()
+
+	g.SetLayout(layout)
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		panic(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		panic(err)
+	}
+}
